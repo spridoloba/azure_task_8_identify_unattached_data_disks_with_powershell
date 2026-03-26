bd628d9a5425cd178d67d8378397d0e512711db8
Serhii <spridoloba@gmail.com>
2026-03-26 23:11:33 +0200

    feat: solution for task

--- task.ps1
+++ task.ps1
@@ -1,1 +1,23 @@
-# Write your code here
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$resourceGroupName = 'mate-azure-task-5'
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$resultPath = Join-Path $scriptDirectory 'result.json'
+
+$disks = Get-AzDisk -ResourceGroupName $resourceGroupName
+
+$unattachedDisks = $disks |
+    Where-Object { $_.DiskState -eq 'Unattached' } |
+    Select-Object `
+        Name,
+        ResourceGroupName,
+        Location,
+        DiskSizeGB,
+        DiskState,
+        ManagedBy,
+        OsType,
+        Sku,
+        Id
+
+$unattachedDisks | ConvertTo-Json -Depth 10 | Out-File -FilePath $resultPath -Encoding utf8
